3761599f4f53c14db13ef09b98c226471b2406a7
cscty <gdufcscty@163.com>
2025-03-12 22:51:47 +0800

    feat: ssr

--- src/components/withClient.js
+++ src/components/withClient.js
@@ -1,26 +1,17 @@
 import { useEffect } from "react";
 import store from "../store";
 export default function (Component) {
-  let count = 0;
-  const newComponent = function (...props) {
-    useEffect(() => {
-      // 客户端渲染，则预获取数据
-      if (window.isCSR) {
-        Component?.getInitData?.(store);
-      } else {
-        // 服务端渲染，服务端预获取过数据一次，
-        // 第二次以后才需获取
-        if (count >= 1) {
-          Component?.getInitData?.(store);
-        } else {
-          count++;
-        }
-      }
-      return () => {};
-    }, []);
+    let count = 0;
+    const newComponent = function (...props) {
+        useEffect(() => {
+            // 客户端渲染，则预获取数据
+            if (window.isCSR) {
+                Component?.getInitData?.(store);
+            }
+        }, []);
 
-    return <Component props={{ ...props }} />;
-  };
-  newComponent.getInitData = Component.getInitData;
-  return newComponent;
+        return <Component props={{ ...props }} />;
+    };
+    newComponent.getInitData = Component.getInitData;
+    return newComponent;
 }
